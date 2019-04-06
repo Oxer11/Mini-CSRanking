86c71b03eb7b236c23c7ecafd1d24df2bcac3ed8
veghen <43173020+veghen@users.noreply.github.com>
2019-04-06 21:16:25 +0800

    Add files via upload

--- CSRanking/urls.py
+++ CSRanking/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 import CSRanking.views as views
 
 urlpatterns = [
-	url(r'^$', views.main, name='main'),
+	url(r'main', views.main, name='main'),
 	url(r'scholar', views.scholar, name='scholar'),
 	url(r'conference', views.conference, name='conference'),
 	url(r'institution', views.institution, name='institution'),
